186dd5cd629d3417023d1a8b8795b15215452802
Ahmed Sharabash <sharabash@debian.sharabash>
2023-04-26 04:36:39 +0200

    fix betty style

--- handle_print.c
+++ handle_print.c
@@ -12,56 +12,55 @@
  * Return: 1 or 2;
  */
 
-
-
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+		 int flags, int width, int precision, int size)
 {
 	int  unknown_size = 0, printed = -1;
 	char c = fmt[*ind];
 
 	if (c == 'c')
-		return print_char(list, buffer, flags, width, precision, size);
+		return (print_char(list, buffer, flags, width, precision, size));
 	else if (c == 's')
-		return print_string(list, buffer, flags, width, precision, size);
+		return (print_string(list, buffer, flags, width, precision, size));
 	else if (c == '%')
-		return print_percent(list, buffer, flags, width, precision, size);
+		return (print_percent(list, buffer, flags, width, precision, size));
 	else if (c == 'i' || c == 'd')
-		return print_int(list, buffer, flags, width, precision, size);
+		return (print_int(list, buffer, flags, width, precision, size));
 	else if (c == 'b')
-		return print_binary(list, buffer, flags, width, precision, size);
+		return (print_binary(list, buffer, flags, width, precision, size));
 	else if (c == 'u')
-		return print_unsigned(list, buffer, flags, width, precision, size);
+		return (print_unsigned(list, buffer, flags, width, precision, size));
 	else if (c == 'o')
-		return print_octal(list, buffer, flags, width, precision, size);
+		return (print_octal(list, buffer, flags, width, precision, size));
 	else if (c == 'x')
-		return print_hexadecimal(list, buffer, flags, width, precision, size);
+		return (print_hexadecimal(list, buffer, flags, width, precision, size));
 	else if (c == 'X')
-		return print_hexa_upper(list, buffer, flags, width, precision, size);
+		return (print_hexa_upper(list, buffer, flags, width, precision, size));
 	else if (c == 'p')
-		return print_address(list, buffer, flags, width, precision, size);
+		return (print_address(list, buffer, flags, width, precision, size));
 	else if (c == 'S')
-		return print_unprinted(list, buffer, flags, width, precision, size);
+		return (print_unprinted(list, buffer, flags, width, precision, size));
 	else if (c == 'r')
-		return print_rev(list, buffer, flags, width, precision, size);
+		return (print_rev(list, buffer, flags, width, precision, size));
 	else if (c == 'R')
-		return print_rot13string(list, buffer, flags, width, precision, size);
+		return (print_rot13string(list, buffer, flags, width, precision, size));
 	else if (c == '\0')
-		return -1;
-	else {
+		return (-1);
+	else
+	{
 		if (fmt[*ind - 1] == ' ')
 			unknown_size += write(1, " ", 1);
-		else if (width) {
+		else if (width)
+		{
 			--(*ind);
 			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
 				--(*ind);
 			if (fmt[*ind] == ' ')
 				--(*ind);
-			return 1;
+			return (1);
 		}
 		unknown_size += write(1, "%%", 1);
 		unknown_size += write(1, &fmt[*ind], 1);
 	}
-	return printed;
+	return (printed);
 }
-
